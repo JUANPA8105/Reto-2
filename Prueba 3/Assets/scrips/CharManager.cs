9680a68cbb26cb01712fce5b97abc08b11d9d711
wilmarmarin <will-marin@hotmail.es>
2023-08-04 00:45:03 -0500

    menu

--- Prueba 3/Assets/scrips/CharManager.cs
+++ Prueba 3/Assets/scrips/CharManager.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class CharManager : MonoBehaviour
 {
-    public static int User = 4;
-    private static bool Start = false;
-    // Update is called once per frame
-    void Update()
+    private int newCharID = 0;
+    public static int charID = 0;
+    public void changeRCharID()
     {
-        if (User != 4)
+        if (newCharID == gameObject.transform.childCount - 1)
         {
-            Start = true;
-            gameObject.SetActive(false);
+            newCharID = 0;
         }
+        else
+        {
+            newCharID += 1;
+        }
+        charID = newCharID;
+        LoadChar();
+    }
+    public void changeLCharID()
+    {
+        if (newCharID == 0)
+        {
+            newCharID = gameObject.transform.childCount - 1;
+        }
+        else
+        {
+            newCharID -= 1;
+        }
+        charID = newCharID;
+        LoadChar();
+    }
+    public void LoadChar()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            if (i == charID)
+            {
+                gameObject.transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                gameObject.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+    private void Start()
+    {
+        LoadChar();
     }
 }
